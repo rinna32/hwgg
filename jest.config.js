f377a91bfeb028e13fac76dd8b42c96e6d297055
rinna32 <r1nnaS1n@yandex.ru>
2025-09-28 21:48:01 +0300

    remove odd file

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,5 @@ module.exports = {
   collectCoverageFrom: [
     "HWGG/hwgg/lp7.js"
   ],
-  coverageThreshold: {
-    global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60,
-    },
-  },
+  
 };
